9cb20a7d968be0d4624d8f53009d60a186adf1bc
gabrielamontescoelho <gabrielamontescoelho@gmail.com>
2026-05-20 21:16:59 -0300

    Configuração inicial do projeto

---
+++ src/main/java/br/com/serratec/segundotrabalhoapi/SegundotrabalhoapiApplication.java
@@ -0,0 +1,13 @@
+package br.com.serratec.segundotrabalhoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundotrabalhoapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundotrabalhoapiApplication.class, args);
+	}
+
+}
